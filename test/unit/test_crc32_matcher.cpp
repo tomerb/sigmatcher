d206132bb8485182c1d8224e29ad00dbb76827f3
Tomer Barletz <barletz@gmail.com>
2023-04-13 15:41:16 +0300

    Use boost/test

--- test/unit/test_crc32_matcher.cpp
+++ test/unit/test_crc32_matcher.cpp
@@ -1,10 +1,22 @@
+#define BOOST_TEST_MODULE sigmatcher_crc32_tests
+#include <boost/test/included/unit_test.hpp>
+
 #include "../../src/signature_matcher_factory.h"
 
 using namespace sigmatcher;
 
-int main()
+BOOST_AUTO_TEST_CASE(check_same_file)
 {
-    auto sig_matcher = SignatureMatcherFactory::Create(SignatureMatcherType::SMT_CRC32);
+    auto sig_matcher =
+        SignatureMatcherFactory::Create(SignatureMatcherType::SMT_CRC32);
     sig_matcher->Add("mal_test.dat");
-    return sig_matcher->Check("ben_test.dat") ? 0 : -1;
+    BOOST_TEST(sig_matcher->Check("mal_test.dat"));
+}
+
+BOOST_AUTO_TEST_CASE(check_different__file)
+{
+    auto sig_matcher =
+        SignatureMatcherFactory::Create(SignatureMatcherType::SMT_CRC32);
+    sig_matcher->Add("mal_test.dat");
+    BOOST_TEST(!sig_matcher->Check("ben_test.dat"));
 }
