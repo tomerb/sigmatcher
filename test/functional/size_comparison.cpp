92c9d22eaa0ac41485ec5ee40788f83a969c6397
Tomer Barletz <barletz@gmail.com>
2023-04-14 15:21:11 +0300

    Initial functional test

---
+++ test/functional/size_comparison.cpp
@@ -0,0 +1,24 @@
+#define BOOST_TEST_MODULE sigmatcher_serializations
+#include <boost/test/included/unit_test.hpp>
+
+#include <filesystem>
+
+#include "../../src/signature_matcher_factory.h"
+
+const std::string BENIGN_DATASET_DIR = "../../tools/benign_dataset";
+const std::string MALICIOUS_DATASET_DIR = "../../tools/malicious_dataset";
+
+using namespace sigmatcher;
+using directory_iterator = std::filesystem::recursive_directory_iterator;
+
+BOOST_AUTO_TEST_CASE(serialize_crc32)
+{
+    auto sig_matcher =
+        SignatureMatcherFactory::Create(SignatureMatcherType::SMT_CRC32);
+    for (const auto& file : directory_iterator(BENIGN_DATASET_DIR))
+    {
+        std::cout << file.path() << std::endl;
+        sig_matcher->Add(file.path());
+    }
+    BOOST_TEST(sig_matcher->Serialize("./crc32.map"));
+}
