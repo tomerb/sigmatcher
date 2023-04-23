80c49b6b1c1d6a4b09f7f76af662f8c902e50056
Tomer Barletz <barletz@gmail.com>
2023-04-23 15:01:35 +0300

    WIP for BF's Add()

--- src/bloom_filter_matcher.cpp
+++ src/bloom_filter_matcher.cpp
@@ -22,13 +22,34 @@ BloomFilterMatcher::BloomFilterMatcher(int number_of_items, double fp_probabilit
     m_bitset.reserve(m_m);
 }
 
-void BloomFilterMatcher::Add(const string &file_path)
+static bool CalcFinalSignature(const string &file_path,
+                               unsigned char murmur[MURMURHASH3_SIZE_BYTES])
 {
     static unsigned char hash[SHA256_SIZE_BYTES];
     if (Utils::Sha256File(file_path, hash))
     {
         auto str_hash = Utils::Sha256ToString(hash);
         cout << str_hash << endl;
+
+        Utils::Murmur3(hash, sizeof(hash), murmur);
+        for (int i = 0; i < m_k; i++)
+        {
+            unsigned char tmp[MURMURHASH3_SIZE_BYTES];
+            Utils::Murmur3(murmur, sizeof(murmur), tmp);
+            memcpy(murmur, tmp, sizeof(tmp));
+        }
+
+        return true;
+    }
+
+    return false;
+}
+
+void BloomFilterMatcher::Add(const string &file_path)
+{
+    static unsigned char murmur[MURMURHASH3_SIZE_BYTES];
+    if (CalcFinalSignature(file_path, murmur))
+    {
     }
 }
 
