60591907f3c154c222bafcc8098047f69f0690c3
Tomer Barletz <barletz@gmail.com>
2023-04-25 00:21:48 +0300

    Improve test

--- test/functional/2_accuracy_perf.cpp
+++ test/functional/2_accuracy_perf.cpp
@@ -16,34 +16,46 @@ static unique_ptr<SignatureMatcher> LoadDbFile(SignatureMatcherType type,
     return sig_matcher;
 }
 
-static void CheckDataset(const unique_ptr<SignatureMatcher> &sig_matcher,
+static bool CheckDataset(const unique_ptr<SignatureMatcher> &sig_matcher,
                          const string &dataset_dir,
                          bool exists)
 {
     BOOST_REQUIRE(filesystem::exists(dataset_dir));
 
     bool has_files = false;
+    int failed_checks = 0;
     for (const auto &file : directory_iterator(dataset_dir))
     {
         has_files = true;
-        BOOST_TEST(sig_matcher->Check(file.path()) == exists);
+        if (sig_matcher->Check(file.path()) != exists)
+        {
+            failed_checks++;
+        }
     }
 
     BOOST_REQUIRE(has_files);
+
+    cout << failed_checks << " tests failed" << endl;
+
+    return failed_checks == 0;
 }
 
-static void CheckAllDatasets(const unique_ptr<SignatureMatcher> &sig_matcher)
+static bool CheckAllDatasets(const unique_ptr<SignatureMatcher> &sig_matcher)
 {
-    CheckDataset(sig_matcher, MALICIOUS_DATASET_DIR, true);
-    CheckDataset(sig_matcher, BENIGN_DATASET_DIR, false);
+    return CheckDataset(sig_matcher, MALICIOUS_DATASET_DIR, true) &&
+        CheckDataset(sig_matcher, BENIGN_DATASET_DIR, false);
 }
 
 BOOST_AUTO_TEST_CASE(test_signatures_crc32)
 {
-    CheckAllDatasets(LoadDbFile(SignatureMatcherType::SMT_CRC32, CRC32_FILENAME));
+    auto sig_matcher =
+        LoadDbFile(SignatureMatcherType::SMT_CRC32, CRC32_FILENAME);
+    BOOST_TEST(CheckAllDatasets(sig_matcher));
 }
 
 BOOST_AUTO_TEST_CASE(test_signatures_bf)
 {
-    CheckAllDatasets(LoadDbFile(SignatureMatcherType::SMT_BLOOM_FILTER, BF_FILENAME));
+    auto sig_matcher =
+        LoadDbFile(SignatureMatcherType::SMT_BLOOM_FILTER, BF_FILENAME);
+    BOOST_TEST(CheckAllDatasets(sig_matcher));
 }
