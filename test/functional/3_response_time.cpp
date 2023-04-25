7a0280ff8894390a9749e3accc1a13eb4e92ab1e
Tomer Barletz <barletz@gmail.com>
2023-04-25 03:19:05 +0300

    Correct code for printouts

--- test/functional/3_response_time.cpp
+++ test/functional/3_response_time.cpp
@@ -3,7 +3,64 @@
 
 #include "test_common.h"
 
-BOOST_AUTO_TEST_CASE(deserialize_crc32)
+static bool CheckDataset(const unique_ptr<SignatureMatcher> &sig_matcher,
+                         const string &dataset_dir,
+                         bool expected)
 {
-    BOOST_TEST(true);
+    BOOST_REQUIRE(filesystem::exists(dataset_dir));
+
+    int total_checks = 0;
+    auto total_time = chrono::nanoseconds::zero();
+    bool had_failures = false;
+
+    for (const auto &file : directory_iterator(dataset_dir))
+    {
+        auto start = chrono::high_resolution_clock::now();
+
+        bool result = sig_matcher->Check(file.path());
+
+        auto end = chrono::high_resolution_clock::now();
+
+        total_time += (end - start);
+        total_checks++;
+
+        if (!result)
+        {
+            had_failures = true;
+        }
+    }
+
+    BOOST_REQUIRE(total_checks > 0);
+
+    std::chrono::duration<double> avg = total_time / total_checks;
+
+    cout << "Conducted " << total_checks << " tests, with average execution time of " <<
+        avg.count() << " seconds)" << endl;;
+
+    return had_failures;
+}
+
+static void LoadAndCheckDataset(const string &dataset_dir, bool expected)
+{
+    {
+        auto sig_matcher =
+            LoadDbFile(SignatureMatcherType::SMT_CRC32, CRC32_FILENAME);
+        BOOST_TEST(CheckDataset(sig_matcher, dataset_dir, expected));
+    }
+
+    {
+        auto sig_matcher =
+            LoadDbFile(SignatureMatcherType::SMT_BLOOM_FILTER, BF_FILENAME);
+        BOOST_TEST(CheckDataset(sig_matcher, dataset_dir, expected));
+    }
+}
+
+BOOST_AUTO_TEST_CASE(test_malicious_dataset)
+{
+    LoadAndCheckDataset(MALICIOUS_DATASET_DIR, true);
+}
+
+BOOST_AUTO_TEST_CASE(test_benign_dataset)
+{
+    LoadAndCheckDataset(BENIGN_DATASET_DIR, false);
 }
